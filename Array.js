22e2c7d503be47d3bb55893a24f2111d2a5a79bf
SINDHU H <yashosindhu@gmail.com>
2023-12-19 16:40:13 -0600

    array.js

---
+++ Array.js
@@ -0,0 +1,15 @@
+let originalvalue=5;
+let newvalue=30;
+originalvalue=newvalue;
+let originalvalue=10;
+
+console.log(originalvalue);
+console.log(newvalue);
+
+let array=[1,2,3,4];
+let newarray=[5,6,7,8];
+array=newarray;
+array.push[9];
+
+console.log(array);
+console.log(newarray);
