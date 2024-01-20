d4158ac4962eda51477afd2ba7883c92e1580131
Sritha Reddy <srithareddy@srithas-air.lan>
2024-01-19 19:24:31 -0500

    exports

--- first.js
+++ first.js
@@ -1,24 +1,25 @@
 
 // //async await
+import  data from "./src/user.js";  
+const name1 = "teja";
 
-
-
-async function getUsers(){
+async function getUsers(i){
        const response = await fetch('https://jsonplaceholder.typicode.com/posts')
        const data = await response.json();
-        const filtereData = data.filter(obj => obj.userId === 2);
+       console.log(data);
+        const filtereData = data.filter(obj => obj.userId === i);
      return filtereData;
        
 }
 
 
-async function createAccordian(){
-    const xx =   await getUsers();
+async function createAccordian(i){
+    const xx =   await getUsers(i);
     const accordian = document.getElementById("accordian");
     const accordianItem =  document.createElement('div');
     accordianItem.classList.add("accordian-item");
     const accordianTitle = document.createElement('div');
-    accordianTitle.innerHTML = "Title"
+    accordianTitle.innerHTML =  `title-${i}`;
     accordianTitle.classList.add("accordian-title");
     const accordianContent = document.createElement('div');
     accordianContent.innerHTML = "content"
@@ -29,11 +30,27 @@ async function createAccordian(){
     accordian.appendChild(accordianItem);
 
     accordianTitle.addEventListener("click", async()=>{
-    
+        accordianContent.innerHTML = xx.map((ele)=>{return ele.title}).join('<br>')
         console.log(xx);
+        
         accordianContent.style.display == 'block' ? accordianContent.style.display = 'none' : accordianContent.style.display = 'block';
     })
 
 }
 
-createAccordian()
+for(let i=1; i<9; i++){
+    createAccordian(i);
+
+}
+
+
+//local storage, session storage, cookie
+
+localStorage.setItem("name", "teja");
+sessionStorage.setItem("time", "evening");
+
+data.printName("teja from first js");
+
+console.log(data.me)
+
+
